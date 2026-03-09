e3577212b4734f1202b5b2772ddc6bfd6b73369b
devLee98 <iiyoungchan@naver.com>
2026-03-09 22:06:59 +0900

    feat:페이지 설정 및 글로벌 레이아웃 설정

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,3 +1,6 @@
+import { SunIcon } from "lucide-react";
+import Image from "next/image";
+import Link from "next/link";
 import "./globals.css";
 import Providers from "./provider";
 
@@ -9,7 +12,35 @@ export default function RootLayout({
   return (
     <html lang="ko">
       <body>
-        <Providers>{children}</Providers>
+        <Providers>
+          <div className="flex min-h-screen flex-col">
+            <header className="h-15 border-b">
+              <div className="m-auto flex h-full w-full max-w-175 justify-between px-4">
+                <Link href={"/"} className="flex items-center gap-2">
+                  <Image src={"/logo.png"} alt="logo" width={20} height={20} />
+                  <div>SNS</div>
+                </Link>
+                <div className="flex items-center gap-5">
+                  <div className="hover:bg-muted cursor-pointer rounded-full p-2">
+                    <SunIcon />
+                  </div>
+                  <Image
+                    src={"/default-avatar.png"}
+                    alt="logo"
+                    width={24}
+                    height={24}
+                  />
+                </div>
+              </div>
+            </header>
+            <main className="m-auto w-full max-w-175 flex-1 border-x px-4 py-6">
+              {children}
+            </main>
+            <footer className="text-muted-foreground border-t py-10 text-center">
+              @이영찬
+            </footer>
+          </div>
+        </Providers>
       </body>
     </html>
   );
--- src/app/provider.tsx
+++ src/app/provider.tsx
@@ -1,10 +1,22 @@
 "use client";
 
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 
 export default function Providers({ children }: { children: React.ReactNode }) {
-  const queryClient = new QueryClient();
+  const queryClient = new QueryClient({
+    //전역으로 설정하는 방법
+    defaultOptions: {
+      queries: {
+        staleTime: 0,
+        gcTime: 5 * 60 * 1000,
+      },
+    },
+  });
   return (
-    <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>
+    <QueryClientProvider client={queryClient}>
+      <ReactQueryDevtools />
+      {children}
+    </QueryClientProvider>
   );
 }
